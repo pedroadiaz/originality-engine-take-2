bd52e77080031bad73e8866abf796658f2544e74
pedroadiaz <pedroadiaz@hotmail.com>
2024-08-10 20:29:08 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import OriginalityEngine from "@/components/OriginalityEngine2/OriginalityEngine";
+
+export default async function Page() {
+  return (
+    <>
+      {/* Render the Builder page */}
+      <OriginalityEngine />
+    </>
+  );
+}
